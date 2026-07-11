d017b4eb608f62352b6e1f7f28debe00ed3b63ce
thegayyogay1-jpg <thegayyogay1@gmail.com>
2026-07-11 22:09:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -94,9 +94,6 @@ app.post('/webhook', async (req, res) => {
                     }
                 }
             }
-                if (userMsg === 'เช็กไอดี') {
-    replyText = `ID ใหม่ของคุณคือ: ${userId}`;
-}
                 // ==================== [ คำสั่งแอดมิน: /set ตั้งค่าราคาและจำนวนช่อง ] ====================
 else if (command === '/s') {
     if (userId !== ADMIN_ID) {
